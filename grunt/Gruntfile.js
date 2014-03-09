1ed93364cfc959882f7e368653904d12dbe3e53c
Kevin Quillen <kevin@inclind.com>
2014-03-09 14:04:55 -0400

    Updating

---
+++ grunt/Gruntfile.js
@@ -0,0 +1,43 @@
+module.exports = function(grunt) {
+  var theme_name = 'bootstrap';
+
+  var global_vars = {
+    theme_name: theme_name,
+    theme_path: '../assets/themes/' + theme_name,
+    theme_css: '../assets/themes/' + theme_name + '/css',
+    theme_scss: '../assets/themes/' + theme_name + '/resources/' + theme_name + '/scss'
+  }
+
+  grunt.initConfig({
+    global_vars: global_vars,
+    pkg: grunt.file.readJSON('package.json'),
+
+    sass: {
+      dist: {
+        options: {
+          outputStyle: 'expanded',
+          sourceComments: 'none',
+          includePaths: ['<%= global_vars.theme_scss %>']
+        },
+        files: {
+          '<%= global_vars.theme_css %>/<%= global_vars.theme_name %>.css': '<%= global_vars.theme_scss %>/styles.scss'
+        }
+      }
+    },
+
+    watch: {
+      grunt: { files: ['Gruntfile.js'] },
+
+      sass: {
+        files: '<%= global_vars.theme_scss %>/**/*.scss',
+        tasks: ['sass']
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-sass');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  grunt.registerTask('build', ['sass']);
+  grunt.registerTask('default', ['watch']);
+}
